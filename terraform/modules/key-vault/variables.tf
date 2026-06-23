ebbb6b83bad362dc5b1c089cfda5850d0aec764f
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-23 12:05:05 +0530

    Create variables.tf

---
+++ terraform/modules/key-vault/variables.tf
@@ -0,0 +1,25 @@
+variable "kv_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+variable "sql_admin_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "environment" {
+  type    = string
+  default = "prod"
+}
