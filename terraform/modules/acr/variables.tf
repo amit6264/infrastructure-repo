0f72cf41bebaee6d2e0fa1b92c148796bf3b3579
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-23 11:52:04 +0530

    Create variables.tf

---
+++ terraform/modules/acr/variables.tf
@@ -0,0 +1,21 @@
+variable "acr_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "sku" {
+  type    = string
+  default = "Premium"
+}
+
+variable "environment" {
+  type    = string
+  default = "prod"
+}
