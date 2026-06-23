1b3f74b2c5f42eec8ce82b4a3dd891cf38aca1c9
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-23 12:35:33 +0530

    Create main.tf

---
+++ terraform/modules/api-management/main.tf
@@ -0,0 +1,31 @@
+#################################################
+# API MANAGEMENT
+#################################################
+
+resource "azurerm_api_management" "this" {
+
+  name                = var.apim_name
+
+  location            = var.location
+
+  resource_group_name = var.resource_group_name
+
+  publisher_name  = var.publisher_name
+
+  publisher_email = var.publisher_email
+
+  sku_name = var.sku_name
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  protocols {
+    enable_http2 = true
+  }
+
+  tags = {
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
+}
