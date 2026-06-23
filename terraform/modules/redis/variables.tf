85f04ff4389051e21f898627b18141440e47fd7b
Amit Patidar <amit.patidar6264@gmail.com>
2026-06-23 12:19:37 +0530

    Create variables.tf

---
+++ terraform/modules/redis/variables.tf
@@ -0,0 +1,31 @@
+variable "redis_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "capacity" {
+  type    = number
+  default = 1
+}
+
+variable "family" {
+  type    = string
+  default = "C"
+}
+
+variable "sku_name" {
+  type    = string
+  default = "Standard"
+}
+
+variable "environment" {
+  type    = string
+  default = "prod"
+}
